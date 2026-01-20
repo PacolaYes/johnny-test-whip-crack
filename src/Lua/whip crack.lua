97177c7e7bb3fed608a3b03611ab0be51aa9031c
Pacola <149804664+PacolaYes@users.noreply.github.com>
2026-01-20 19:37:40 -0300

    add Customizability TM

--- src/Lua/whip crack.lua
+++ src/Lua/whip crack.lua
@@ -3,18 +3,23 @@
 ---@field laststate statenum_t?
 ---@field whip_delay tic_t?
 
-local WHIP_DELAY = TICRATE/2
+local whipdelay = CV_RegisterVar({
+    name = "whipcrack_delay",
+    defaultvalue = "0.3",
+    PossibleValue = CV_Unsigned,
+    flags = CV_FLOAT|CV_NETVAR
+})
+
 sfxinfo[freeslot("sfx_jtwcr")].caption = "Whip Crack" -- we REALLY need captions for this.
 
 ---@param mo mobj_t
 local function handleWhipCrack(mo)
     if not (mo and mo.valid) then return end
 
-    if mo.laststate ~= nil
-    and mo.state ~= mo.laststate
+    if mo.state ~= mo.laststate
     and not mo.whip_delay then
         S_StartSound(mo, sfx_jtwcr)
-        mo.whip_delay = WHIP_DELAY
+        mo.whip_delay = FixedRound(TICRATE * whipdelay.value)/FU
     end
 
     mo.laststate = mo.state
