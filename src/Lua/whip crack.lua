68d8e088e8fc3f1650ba5566963695c587257a0f
Pacola <149804664+PacolaYes@users.noreply.github.com>
2026-01-20 19:10:04 -0300

    whip crack for EVERYONE!!
    or everyone that a player is like, 512 FUs in distance of :P

--- src/Lua/whip crack.lua
+++ src/Lua/whip crack.lua
@@ -1,16 +1,34 @@
 
----@class player_t
----@field laststate statenum_t
+---@class mobj_t
+---@field laststate statenum_t?
+---@field whip_delay tic_t?
 
-sfxinfo[freeslot("sfx_jtwcr")].caption = "///"
+local WHIP_DELAY = TICRATE/2
+sfxinfo[freeslot("sfx_jtwcr")].caption = "Whip Crack" -- we REALLY need captions for this.
 
 ---@param p player_t
 addHook("PlayerThink", function(p)
+    searchBlockmap("objects", function(_, mo) -- cuz we hate MF_NOBLOCKMAP objects
+        if not (mo and mo.valid) then return end
 
-    if p.laststate ~= nil
-    and p.realmo.state ~= p.laststate then
-        S_StartSound(p.mo, sfx_jtwcr)
+        if mo.laststate ~= nil
+        and mo.state ~= mo.laststate
+        and not mo.whip_delay then
+            S_StartSound(mo, sfx_jtwcr)
+            mo.whip_delay = WHIP_DELAY
+        end
+
+        mo.laststate = mo.state
+        mo.whip_delay = $ and $-1 or 0
+    end, p.realmo, p.realmo.x - 512*FU, p.realmo.x + 512*FU, p.realmo.y - 512*FU, p.realmo.y + 512*FU)
+
+    if p.realmo.laststate ~= nil
+    and p.realmo.state ~= p.realmo.laststate
+    and not p.realmo.whip_delay then
+        S_StartSound(p.realmo, sfx_jtwcr)
+        p.realmo.whip_delay = WHIP_DELAY
     end
 
-    p.laststate = p.realmo.state
+    p.realmo.laststate = p.realmo.state
+    p.realmo.whip_delay = $ and $-1 or 0
 end)
